18a57754087c2dcd89562dc993452a80bb7f805a
Shelby Smitherman <smithermansl@gmail.com>
2019-02-04 16:06:54 -0500

    setup init heroku deployment

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const port = process.env.PORT || 3000;
-// const { db } = require('./db');
 
 const app = express();
 
@@ -15,12 +14,16 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// router
-app.use('/api', require('./api'))
-
-app.use((err, req, res) => {
-  console.log(err)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
+app.get('/', (req, res) => {
+  res.send('Please use /api to access API endpoints.');
 })
 
-app.listen(port, () => console.log(`Server started on port: ${port}`))
+// router
+app.use('/api', require('./api'));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(port, () => console.log(`Server started on port: ${port}`));
