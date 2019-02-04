5007550da984dba7f5f48ec209e7fd441168fa08
Shelby Smitherman <smithermansl@gmail.com>
2019-02-04 17:31:51 -0500

    production db url

--- db/database.js
+++ db/database.js
@@ -3,9 +3,8 @@
 // establish connection to PG database
 
 const Sequelize = require('sequelize');
+const localDb = 'postgres://localhost:5432/archive';
 
-const db = new Sequelize('postgres://localhost:5432/archive', {
-  logging: false
-});
+const db = new Sequelize(process.env.DATABASE_URL || localDb, { logging: false });
 
 module.exports = db;
